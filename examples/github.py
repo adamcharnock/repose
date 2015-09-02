b825ffa69ebe2fbcfc0576f5cdf7470431cc0f9f
Adam Charnock <adam@adamcharnock.com>
2015-09-03 00:39:24 +0100

    Adding comments to example

--- examples/github.py
+++ examples/github.py
@@ -19,7 +19,7 @@ class Repository(Resource):
     has_issues = fields.Boolean()
     has_wiki = fields.Boolean()
     has_downloads = fields.Boolean()
-
+    # Will contain a User resource
     owner = fields.Embedded(User)
 
     class Meta:
@@ -28,11 +28,15 @@ class Repository(Resource):
 
 
 if __name__ == '__main__':
+    # Create the api and register our resources
     github_api = Api(base_url='https://api.github.com/')
     github_api.register_resource(User)
     github_api.register_resource(Repository)
 
+    # Get a specific repository. The keys correspond to the
+    # placeholders in the endpoints specified above.
     repo = Repository.objects.get(user_login='adamcharnock', repository_name='repose')
+    # Print some information about it
     print("{} - {}".format(repo.name, repo.description))
     print("Homepage: {}".format(repo.homepage))
     print("Owner avatar: {}".format(repo.owner.avatar_url))
